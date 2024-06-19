3a006e4c3c0f15e20d70ee5050c0959387f87b09
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-19 22:00:49 +0530

    2. definition of callback

---
+++ js/script.js
@@ -0,0 +1,34 @@
+/* fetch API = that requires the discussion of callbacks, 
+promises,thenables and async/await */
+
+// callbacks = are the functions that are passed to the other functions as parameters 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
