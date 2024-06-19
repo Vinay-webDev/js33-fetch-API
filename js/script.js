1b2a43239c939991544e7330e6a0fa92e836ca21
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-06-19 22:11:34 +0530

    3. "callback hell"

--- js/script.js
+++ js/script.js
@@ -3,13 +3,24 @@ promises,thenables and async/await */
 
 // callbacks = are the functions that are passed to the other functions as parameters 
 
+// example
 
+function firstFunction(parameter, callback) {
+    // do stuff
+    callback();
+}
 
+// "callback hell"
 
+firstFunction(para, function() {
+    // do stuff
+    secondFunction(para, function() {
+        // do stuff
+        thirdFunction(para, function() {
 
-
-
-
+        })
+    })
+})
 
 
 
